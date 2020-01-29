b0b51b6bd78e5d65a9359c4c2c3810e7b1527b49
TechQuery <shiy007@qq.com>
2020-01-30 00:32:05 +0800

    【迁移】代码库名 【修正】Workbox 版本

--- workbox-config.js
+++ workbox-config.js
@@ -3,5 +3,7 @@ module.exports = {
     globPatterns: ['**/*.{css,js,ico,gif,png,jpg,jpeg,webp}'],
     swDest: 'dist/sw.js',
     importWorkboxFrom: 'disabled',
-    importScripts: ['https://cdn.jsdelivr.net/npm/workbox-sw']
+    importScripts: [
+        'https://cdn.jsdelivr.net/npm/workbox-sw@4.3.1/build/workbox-sw.min.js'
+    ]
 };
