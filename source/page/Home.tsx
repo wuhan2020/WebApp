34ca184df1bfe166352100c1f00aee8cee697433
Cold Stone <xiaoranran1993@outlook.com>
2020-01-30 15:19:20 +0800

    feat: update hame layout

--- source/page/Home.tsx
+++ source/page/Home.tsx
@@ -1,32 +1,33 @@
 import { createCell, Fragment } from 'web-cell';
 import { Jumbotron } from 'boot-cell/source/Content/Jumbotron';
-import { Card } from 'boot-cell/source/Content/Card';
 
 import menu from './menu';
 
 export function HomePage() {
     return (
         <Fragment>
-            <Jumbotron
-                title="2020 援助武汉"
-                description="新冠病毒疫情中的武汉援助信息网站"
-            />
-            <div className="container card-deck m-auto justify-content-around">
-                {menu.slice(1, -1).map(({ title, href, icon }) => (
-                    <Card
-                        className="mx-auto mt-4 mx-sm-1 text-center"
-                        style={{ minWidth: '14rem', maxWidth: '14rem' }}
-                        title={title}
-                        image={
-                            <a href={href}>
+            <section className="container">
+                <Jumbotron
+                    title="2020 援助武汉"
+                    description="新冠病毒疫情中的武汉援助信息网站"
+                />
+                <ul className="row" style={{ listStyle: 'none', padding: '0' }}>
+                    {menu.slice(1, -1).map(({ title, href, icon }) => (
+                        <li class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+                            <a
+                                className="card text-center"
+                                style={{ marginBottom: '1rem' }}
+                                href={href}
+                            >
                                 <i
                                     className={`fa fa-5x fa-${icon} mt-4 mb-2`}
                                 />
+                                <p>{title}</p>
                             </a>
-                        }
-                    />
-                ))}
-            </div>
+                        </li>
+                    ))}
+                </ul>
+            </section>
         </Fragment>
     );
 }
