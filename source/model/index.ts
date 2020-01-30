f3b3644eb566f7d49ac8216c660c960d674388d9
MR.M <1664817185@qq.com>
2020-01-30 17:11:07 +0800

    clinic列表页UI

--- source/model/index.ts
+++ source/model/index.ts
@@ -3,14 +3,17 @@ import { History } from 'cell-router/source';
 import { Session } from './Session';
 import { SuppliesRequirementModel } from './SuppliesRequirement';
 import { LogisticsModel } from './Logistics';
+import { ClinicModel } from './Clinic';
 
 export const history = new History();
 export const session = new Session();
 export const suppliesRequirement = new SuppliesRequirementModel();
 export const logistics = new LogisticsModel();
+export const clinic = new ClinicModel();
 
 export * from './HTTPService';
 export * from './GitService';
 export * from './AMap';
 export * from './SuppliesRequirement';
 export * from './Logistics';
+export * from './Clinic';
