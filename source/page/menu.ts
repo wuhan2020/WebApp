d8ed5d0c7fb6a95be8687990104439a179d49901
xrkffgg <xrkffgg@vip.qq.com>
2020-01-31 21:38:07 +0800

    remove font space

--- source/page/menu.ts
+++ source/page/menu.ts
@@ -9,36 +9,36 @@ export enum RouteRoot {
 
 export default [
     {
-        title: '首 页',
+        title: '首页',
         href: ''
     },
     {
-        title: '医 院',
+        title: '医院',
         href: RouteRoot.Hospital,
         icon: 'hospital'
     },
     {
-        title: '物 流',
+        title: '物流',
         href: RouteRoot.Logistics,
         icon: 'shipping-fast'
     },
     {
-        title: '酒 店',
+        title: '酒店',
         href: RouteRoot.Hotel,
         icon: 'hotel'
     },
     {
-        title: '生 产',
+        title: '生产',
         href: RouteRoot.Factory,
         icon: 'medkit'
     },
     {
-        title: '捐 赠',
+        title: '捐赠',
         href: RouteRoot.Donation,
         icon: 'heart'
     },
     {
-        title: '义 诊',
+        title: '义诊',
         href: RouteRoot.Clinic,
         icon: 'user-md'
     },
